a523d3651c3c41ceb9fe8cbabd7bb333187f2dbb
sanket-hanchate <144404942+sanket-hanchate@users.noreply.github.com>
2025-09-01 22:35:27 +0530

    Add files via upload

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,1 +1,30 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import { ARCJET_KEY } from "./env.js";
+const aj = arcjet({
+    key: ARCJET_KEY,
+    rules: [
+        // Shield protects your app from common attacks e.g. SQL injection
+        shield({ mode: "LIVE" }),
+        // Create a bot detection rule
+        detectBot({
+            mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+            // Block all bots except the following
+            allow: [
+                "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+                // Uncomment to allow these other common bot categories
+                // See the full list at https://arcjet.com/bot-list
+                //"CATEGORY:MONITOR", // Uptime monitoring services
+                //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+            ],
+        }),
+        // Create a token bucket rate limit. Other algorithms are supported.
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 5,
+            interval: 10, 
+            capacity: 10,
+        }),
+    ],
+});
 
+export default aj
